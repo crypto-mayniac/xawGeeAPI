cf6633e7375d27b28fa2eb7c9e5d8df11784e960
crypto-mayniac <cryptomayniac@proton.me>
2024-11-21 19:37:20 -0700

    Swithc port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
