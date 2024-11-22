abfacfc6bd87298aa374701f6ed993812dea7e4e
crypto-mayniac <cryptomayniac@proton.me>
2024-11-21 19:42:25 -0700

    Whats goin on

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('Webhook received successfully');
 });
 
+app.post('/', (req, res) => {
+    console.log('Webhook received:', req.body);
+    res.status(200).send('Got base webhook');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
